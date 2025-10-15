497d1cbcb76198b819f92011363c4a5743e5b75a
Nikhil Lande <nikhillande9@gmail.com>
2025-10-15 14:44:38 +0530

    Init

---
+++ scenes/player.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@export var speed := 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	position = Vector2(640,600)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var direction = Input.get_vector("left","right","up","down")
+	position += direction * speed * delta
+	
